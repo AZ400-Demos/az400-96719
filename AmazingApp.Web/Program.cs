0f1d6d70ca9a222c9e9cc7a3401bbd6685ac56a6
Massimo Bonanni <massimo.bonanni@tiscali.it>
2026-04-10 09:39:30 +0200

    Add DESCryptoServiceProvider for encryption in Program.cs

--- AmazingApp.Web/Program.cs
+++ AmazingApp.Web/Program.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -25,5 +27,9 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+
+var des = new DESCryptoServiceProvider(); // 🔥 Weak algorithm
+des.GenerateKey();
+des.GenerateIV();
 
 app.Run();
